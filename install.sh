085bc4fb4758e8f86e7cbe2976edfb5862ee83a6
Brian Stone <brians@zillow.com>
2014-05-23 12:11:05 -0700

    Add quick install script.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+go()
+{
+    PWD=`pwd`
+    rm -rf ~/.emacs
+    rm -rf ~/.emacs.d
+    ln -s $PWD/.emacs ~
+    ln -s $PWD/.emacs.d ~
+    echo "Installed."
+}
+
+if [ -a ~/.emacs -o -a ~/.emacs.d ]; then
+    read -p "Overwrite existing emacs dot files? " response
+    case $response in
+        [Yy]*)
+            go;;
+        *)
+            echo "Aborted."
+            exit;;
+    esac
+fi
